1eb841f0560a49295509ec6d9c60e9944271bda9
Wilson635 <87822542+Wilson635@users.noreply.github.com>
2025-03-06 14:36:29 +0100

    tutoriel sur l'affichage du text et type de données

---
+++ csharp/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace csharp;
+using System;
+
+class Csharp
+{
+    static void Main()
+    {
+        Console.Title="My First Game";
+
+        // Déclaration des variables
+        Double a = 12.5;
+        Double b = 12.5;
+
+        Double c = a + b;
+
+        string text = "wilson";
+        string subText = "Steve";
+        
+        // Affichage du text
+        Console.WriteLine(text + ' ' + subText);
+        
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+        Console.WriteLine(c);
+    }
+}
