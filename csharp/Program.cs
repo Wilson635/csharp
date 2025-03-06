1394be8c4be3c4255e10fcae9c01e851e3ef6ce6
Wilson635 <87822542+Wilson635@users.noreply.github.com>
2025-03-06 17:39:58 +0100

    install package figgle and use it to modify font about user entering

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -18,7 +18,7 @@ class Csharp
     {
         Console.Title = "My First Game";
         /*
-            // Déclaration des variables et Types de données
+            // Déclaration des variables et Types de données.
             Double a = 12.5;
             Double b = 12.5;
 
@@ -27,16 +27,26 @@ class Csharp
             string text = "wilson";
             string subText = "Steve";
 
-            // Affichage du text
+            // Affichage du resultat.
             Console.WriteLine(text + ' ' + subText);
+            Console.WriteLine($"{text} {subText}"); // Autre méthode de concaténation.
 
             Console.WriteLine(a);
             Console.WriteLine(b);
             Console.WriteLine(c);
+
+            // Utilisation des opérateurs.
+            bool result = (5 % 2 == 0);
+            Console.WriteLine(result);
+
+            // Utilisation des conditions et opérateur ternaire .
+            Console.WriteLine("Enter your name: ");
+            string? name = Console.ReadLine();
+
+            Console.WriteLine(name == null
+                ? "Please enter your name: "
+                : Figgle.FiggleFonts.Standard.Render($"Welcome to {name}"));
         */
-        
-        bool result = (5 % 2 == 0);
-        
-        Console.WriteLine(result);
+
     }
 }
